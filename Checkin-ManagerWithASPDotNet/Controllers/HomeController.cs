c77c50221496102228fc562d08e57686b3a0c341
porinut <porinut1@gmail.com>
2019-03-17 14:56:17 +0700

    first commit

---
+++ Checkin-ManagerWithASPDotNet/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Checkin_ManagerWithASPDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page."; //message is sent
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page."; //container
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Your Test page.";
+
+            return View();
+        }
+
+        public ActionResult Test2()
+        {
+            ViewBag.Message = "Your Test page.";
+
+            return View("Test2");
+        }
+
+
+
+
+    }
+}
---
+++ Checkin-ManagerWithASPDotNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Checkin_ManagerWithASPDotNet.MvcApplication" Language="C#" %>
